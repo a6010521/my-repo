6b78595651a8ef2e8cdb7a67cdc9d8d8de532dbb
Victor <a6010521@gmail.com>
2025-01-21 14:27:06 +0800

    語法測試

--- load_json.py
+++ load_json.py
@@ -1,21 +1,32 @@
-from pathlib import Path
 import pandas as pd
+import requests
 import json
+import csv
 
-def json_load():
-    readDir = Path("readjson")
-    # 讀取JSON文件
-    dfJSON = pd.read_json(readDir/"film.json")
-    print(f"dfJSON:\n{dfJSON}")
+url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_year=2024&sort_by=popularity.desc"
+
+headers = {
+"accept": "application/json",
+"Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjMzNGEwZGJiNjU0NDIzMjlkODg4NWQxNjMzZGZhNSIsIm5iZiI6MTczNjkzOTQ2NS4zNDgsInN1YiI6IjY3ODc5N2M5YmQ3OTNjMDM1NDRmMjBlNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.c0Fm2RPV9hH_cZ6t_7PTSw5HTjGuKZSQDbeU5c-D99c"
+	}
+
+response = requests.get(url, headers=headers)
+tw_movie_2024 = response.text
+
+with open("movie_2024.json", "w", encoding="utf-8") as f:
+	json.dump(tw_movie_2024, f, ensure_ascii=False)
+print('存取成功') 
 
 
-    df = pd.DataFrame(dfJSON)
 
-# 將 DataFrame 存成 Excel
-    excel_file = "movies_data.xlsx"
-    df.to_excel(excel_file, index=False)
 
-    print(f"JSON 資料已成功轉存為 Excel 檔案：{excel_file}")
+
+
+
+
+
+
+
 
 
 
